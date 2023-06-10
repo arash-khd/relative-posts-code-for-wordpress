6568fa5f8fbbaebc496d6c3f0b2ce3f9e2b945a8
arash-khd <126689940+arash-khd@users.noreply.github.com>
2023-06-10 16:22:04 +0330

    Update relative_posts.php

--- relative_posts.php
+++ relative_posts.php
@@ -17,4 +17,19 @@ $args = array(
 );
 $query = new WP_Query($args);
 
+//based on categories
+
+$categories = get_the_category();
+  foreach($categories as $category){
+    $rel_cat[] = $category->cat_ID;
+  }
+  $rep_args = array(
+    'post__not_in' => array($postid), 
+    'category__in' => $rel_cat, 
+    'posts_per_page' => 8, 
+    'orderby' => 'rand' 
+  );
+
+  $rep_query = new wp_query($rep_args);
+
 ?>
